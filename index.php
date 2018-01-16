055aa9e4f9a029da358aec9b22ec535545c90851
unknown <lessire.florent@gmail.com>
2018-01-16 16:41:32 +0100

    creation style.css et intégration de bootstrap

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 ﻿<!DOCTYPE html>
-<html>
-    <head>
-        <!-- En-tête de la page -->
-        <meta charset="utf-8" />
-        <title>Sos-Compunter</title>
-    </head>
+<html lang="fr">
+  <head>
+    <title>Sos-Compunter</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- lib icone-->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<!-- bootstrap core css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- custome template -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
 
-    <body>
-        <!-- Corps de la page -->
-    </body>
+  </body>
 </html>
